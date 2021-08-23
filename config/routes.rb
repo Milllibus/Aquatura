ed0fda378d10061db4c426f8da26114e49088b31
adrien leclercq <leclercq.adrien96@gmail.com>
2021-08-23 17:59:13 +0200

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :plants, only: %i[create show destroy]
+  resources :species, only: %i[index show]
+  resources :waterings, only: %i[create]
+  resources :eplants, only: %i[new create show update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
