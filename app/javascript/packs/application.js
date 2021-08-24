307493121a90d4c91bbe7a3be85066c875bac5a8
thais de rodez <thaisderodez@gmail.com>
2021-08-24 12:58:08 +0200

    finished navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,10 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { nav_menu } from "../plugins/_navbar_menu"
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  nav_menu();
 });
