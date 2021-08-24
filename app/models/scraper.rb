0940ffa302daeeafb7f4b7665698f969d5eb9086
Rogerio DaSilva <rogerioa.farinha@gmail.com>
2021-08-24 15:13:20 +0200

    scrapping working

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -4,13 +4,12 @@ require 'open-uri'
 
 class Scraper
   def scrap
-    plant_name = 'jacinthe'
+    plant_name = "jacinthe"
     url = "https://jardinage.ooreka.fr/plante/recherche?motsClefs=#{plant_name}"
-    ​
     html_file = URI.open(url).read
     html_doc = Nokogiri::HTML(html_file)
     ​
-    plant = html_doc.search('.info_plante').first
+    plant = html_doc.search(".info_plante").first
     plant_name = plant.text.strip
     plant_watering = plant.search('.caracteristique')[1].search('span').attr('class').value
     ​
