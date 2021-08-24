86c33666f800638a44daaf82da6503cd0ba2890e
Augustin Clot <augustin.clot@gmail.com>
2021-08-24 14:22:34 +0200

    create done but to be retouched regarding specie_id, need to redo routes for species

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -4,5 +4,25 @@ class PlantsController < ApplicationController
     @plants = Plant.where(user_id: current_user)
   end
 
+  def create
+    @plant = Plant.new(plant_params)
+    @plant.user = current_user
+    if @plant.save
+      redirect_to plant_path
+    else
+      redirect_to specie_path
+    end
+  end
 
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:nickname, :exposure)
+  end
 end
