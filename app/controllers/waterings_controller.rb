5868aea6f540f749b35fb48461760099d8a4f1db
adrien leclercq <leclercq.adrien96@gmail.com>
2021-08-24 12:40:55 +0200

    Add watering create

--- app/controllers/waterings_controller.rb
+++ app/controllers/waterings_controller.rb
@@ -1,2 +1,18 @@
 class WateringsController < ApplicationController
+  def create
+    @plant = Plant.find(params[:id])
+    @watering = Watering.new
+    @watering.plant = @plant
+    @watering.save
+    watering_eplant
+  end
+
+  private
+
+  def watering_eplant
+    return if @plant.user.eplant.nil?
+
+    @eplant = @plant.user.eplant
+    @eplant.level += 10
+  end
 end
