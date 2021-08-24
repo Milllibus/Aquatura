49cfdb4f01614ba59a7f6e55dc5303ab5df1b314
Augustin Clot <augustin.clot@gmail.com>
2021-08-24 14:28:08 +0200

    routes for species index and show modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :plants, only: %i[create show destroy]
-  resources :species, only: %i[index show]
+  resources :plants, only: %i[index create show destroy]
+  get '/species', to: 'species#index'
+  get '/species/:id', to: 'species#show', as: 'specie'
   resources :waterings, only: %i[create]
   resources :eplants, only: %i[new create show update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
