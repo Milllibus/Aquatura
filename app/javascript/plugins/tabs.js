098c912016c7d4e53db3dcda08f35ea3d8a9edc3
Augustin Clot <augustin.clot@gmail.com>
2021-08-25 15:43:55 +0200

    tabs working need style

--- app/javascript/plugins/tabs.js
+++ app/javascript/plugins/tabs.js
@@ -1,13 +1,17 @@
 const tab_opener = () => {
   const tabs = document.querySelectorAll(".tablinks");
-  if (tabs) {
-    tabs.forEach(tab => {
-      tab.addEventListener('click', event => {
-        console.log('salut');
-      });
-    });
-  }
-
+  const contents = document.querySelectorAll('.tabcontent');
+  tabs.forEach(tab => {
+    tab.addEventListener('click', event => {
+      contents.forEach(content => {
+        if (content.id === event.currentTarget.innerText) {
+          content.classList.add('active');
+         } else {
+          content.classList.remove('active');
+         }
+      })
+    })
+  })
 }
 
 export { tab_opener };
