65aef4cf6ca6098de9c659405cb8f4ea17ab83d3
Augustin Clot <augustin.clot@gmail.com>
2021-08-26 11:20:38 +0200

    added an if in the js pluggin tabs.js

--- app/javascript/plugins/tabs.js
+++ app/javascript/plugins/tabs.js
@@ -1,24 +1,26 @@
 const tab_opener = () => {
   const tabs = document.querySelectorAll(".tablinks");
   const contents = document.querySelectorAll('.tabcontent');
-  tabs.forEach(tab => {
-    tab.addEventListener('click', event => {
-      contents.forEach(content => {
-        if (content.id === event.currentTarget.dataset.tab) {
-          content.classList.add('active');
-          event.currentTarget.classList.add('active-btn');
-          // tabs.forEach(tab => {
-          //   if (content.className !== event.currentTarget.innerText) {
-          //     tab.classList.remove('active-btn');
-          //   };
-          // });
-        } else {
-          content.classList.remove('active');
-          document.querySelector(`button.tablinks[data-tab="${content.id}"]`).classList.remove('active-btn');
-        };
+  if (tabs) {
+    tabs.forEach(tab => {
+      tab.addEventListener('click', event => {
+        contents.forEach(content => {
+          if (content.id === event.currentTarget.dataset.tab) {
+            content.classList.add('active');
+            event.currentTarget.classList.add('active-btn');
+            // tabs.forEach(tab => {
+            //   if (content.className !== event.currentTarget.innerText) {
+            //     tab.classList.remove('active-btn');
+            //   };
+            // });
+          } else {
+            content.classList.remove('active');
+            document.querySelector(`button.tablinks[data-tab="${content.id}"]`).classList.remove('active-btn');
+          };
+        });
       });
     });
-  });
+  };
 };
 
 export { tab_opener };
