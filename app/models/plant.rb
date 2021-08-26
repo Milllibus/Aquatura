9bb339132a19f681a4f4a47001db92c4e76f1762
Rogerio DaSilva <rogerioa.farinha@gmail.com>
2021-08-26 12:12:06 +0200

    form buttons

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,8 @@
 class Plant < ApplicationRecord
+  EXPOSURES = %w[sun half shaddow]
   validates :nickname, presence: true
-  validates :exposure, presence: true
+  validates :exposure, presence: true, inclusion: EXPOSURES
+
   belongs_to :specie
   belongs_to :user
   has_many :waterings, dependent: :destroy
