59859d9a5043b0d906f28d4bbe24a8bb1e997fed
adrien leclercq <leclercq.adrien96@gmail.com>
2021-08-27 14:01:02 +0200

    correct show plant

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -19,6 +19,7 @@ class PlantsController < ApplicationController
   def show
     @plant = Plant.find(params[:id])
     @specie = @plant.specie
+    @next_day = (@plant.dates_of_watering(30).first - Date.today).to_i
     authorize @plant
   end
 
