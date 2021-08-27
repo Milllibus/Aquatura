904e98073a698ac163140a08eae01f52fee91e1f
Augustin Clot <augustin.clot@gmail.com>
2021-08-27 12:22:53 +0200

    in user model hasoneattached photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :plants, dependent: :destroy
   has_one :eplant, dependent: :destroy
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
