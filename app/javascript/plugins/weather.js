7915132a20b2bca0b3705e3cdc7c5d1c04725a32
Rogerio DaSilva <rogerioa.farinha@gmail.com>
2021-08-30 23:18:39 +0200

    weather done

--- app/javascript/plugins/weather.js
+++ app/javascript/plugins/weather.js
@@ -1,11 +1,26 @@
+const weather = () => {
 
-const weather = (query) => {
+  const temperature = document.querySelector('#temperature');
+  const description = document.querySelector('#description');
+  const icon = document.querySelector('#icon');
+  const city = document.querySelector('#city');
 
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=marseille&appid=3a84afc0cbb1319754a397547de8202e&units=metric`)
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data);
-      });
-};
+  const updateCard = (data) => {
+    description.innerText = data.weather[0].description;
+    temperature.innerText = `${Math.round(data.main.temp)}°C`;
+    icon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    city.innerText = data.name;
+  };
 
+  navigator.geolocation.getCurrentPosition((data) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${data.coords.latitude}&lon=${data.coords.longitude}&appid=3a84afc0cbb1319754a397547de8202e&units=metric`
+    fetch(url)
+      .then(response => response.json())
+      .then(updateCard);
+
+  });
+
+
+
+}
 export { weather };
