ac70d79233b5fdc00c984401828663b47a29beba
Rogerio DaSilva <rogerioa.farinha@gmail.com>
2021-08-31 10:12:54 +0200

    WIP on displaying advice

--- app/javascript/plugins/weather.js
+++ app/javascript/plugins/weather.js
@@ -5,11 +5,33 @@ const weather = () => {
   const icon = document.querySelector('#icon');
   const city = document.querySelector('#city');
 
+  const advices = [
+    {
+      min: 0,
+      max: 10,
+      advice: "put your plants in th fire"
+    },
+    {
+      min: 10,
+      max: 25,
+      advice: "lidjkjdvf"
+    },
+    {
+      min: 25,
+      max: 100,
+      advice: "loijljkl"
+    },
+  ]
   const updateCard = (data) => {
     description.innerText = data.weather[0].description;
-    temperature.innerText = `${Math.round(data.main.temp)}°C`;
+    const tempCelcius = Math.round(data.main.temp);
+    temperature.innerText = `${tempCelcius}°C`;
     icon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     city.innerText = data.name;
+    // get the advice depending on temp
+    console.log(tempCelcius);
+    const adviceTodisplay = advices.find((adviceObj) => {return 102 >= adviceObj.min && 102 < adviceObj.max })?.advice;
+    console.log(adviceTodisplay);
   };
 
   navigator.geolocation.getCurrentPosition((data) => {
