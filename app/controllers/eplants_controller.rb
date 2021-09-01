9e0163e3447f33b4ec57cf51e7aad895c82f2cef
Augustin Clot <augustin.clot@gmail.com>
2021-09-01 15:18:36 +0200

    checked routes new create, created methods new and create in controller

--- app/controllers/eplants_controller.rb
+++ app/controllers/eplants_controller.rb
@@ -1,6 +1,26 @@
 class EplantsController < ApplicationController
+  def new
+    @plant = Eplant.new
+  end
+
+  def create
+    @eplant = Eplant.new(eplant_params)
+    @eplant.user = current_user
+    if @eplant.save
+      redirect_to eplant_path(@eplant)
+    else
+      render :new
+    end
+  end
+
   def show
     @eplant = current_user.eplant
     authorize @eplant
+  end
+
+  private
+
+  def eplant_params
+    params.require(:eplant).permit(:nickname)
   end
 end
