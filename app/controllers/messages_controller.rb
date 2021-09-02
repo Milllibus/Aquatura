6f4bb7b2f51c0bc7c70573f4bdfb9a36524300b5
thais de rodez <thaisderodez@gmail.com>
2021-09-02 12:55:31 +0200

    fixed chat display

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,13 @@ class MessagesController < ApplicationController
     @message.chatroom = @chatroom
     @message.user = current_user
     if @message.save
-      ChatroomChannel.broadcast_to(@chatroom, render_to_string(partial: "message", locals: { message: @message }))
+      data_str = render_to_string(partial: "message", locals: { message: @message })
+      data = {
+        content: data_str,
+        author_id: @message.user.id,
+        message_div_id: "#message-#{@message.id}"
+      }
+      ChatroomChannel.broadcast_to(@chatroom, data)
       redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
     else
       render "chatrooms/show"
--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,13 +1,24 @@
 import consumer from "./consumer";
 
 const initChatroomCable = () => {
-  const messagesContainer = document.getElementById('messages');
+  let messagesContainer = document.getElementById('messages');
   if (messagesContainer) {
     const id = messagesContainer.dataset.chatroomId;
 
     consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
       received(data) {
-        messagesContainer.insertAdjacentHTML('beforeend', data);
+        // let messages = document.getElementById('messages');
+        // console.log(messages);
+        console.log(data);
+        if (!document.querySelector(`${data.message_div_id} .message-container`)) {
+          document.getElementById('messages').insertAdjacentHTML('beforeend', data.content);
+          if (data.author_id !== document.getElementById('messages').dataset.userId) {
+            document.querySelector(`${data.message_div_id} .message-container`).classList.remove('align-right');
+          }
+          document.querySelector(`${data.message_div_id} .message-container`).scrollIntoView({ behavior: "smooth" });
+        }
+        // console.log(data.message_div_id + " .message-container")
+        // let messageDiv = document.querySelector(data.message_div_id + " .message-container")
       }
     });
   }
